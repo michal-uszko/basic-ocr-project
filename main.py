13b1ecdf3a023745a3270791776cf6d47ab0e912
MichalUszko <michal.uszko1@gmail.com>
2021-08-24 20:59:34 +0200

    Adding comments and changin video capture function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.models import load_model
 from imutils.contours import sort_contours
-from imutils.video import VideoStream, FPS
+from imutils.video import FPS
 import numpy as np
 import imutils
 import argparse
@@ -21,18 +21,20 @@ model = load_model(args["model"])
 labels = pickle.loads(open(args["labels"], 'rb').read())
 
 print("[INFO] Starting video stream...")
-vs = VideoStream(src=0).start()
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 time.sleep(2.0)
 fps = FPS().start()
 
 while True:
-    frame = vs.read()
+    ret, frame = cap.read()
     frame = imutils.resize(frame, width=1200)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     kernel = np.ones((3, 3))
     edged = cv2.Canny(blurred, 165, 80)
 
+    # Finding and sorting contours left to right, error handling prevents
+    # script's crashes in case of no contours were detected
     try:
         contours = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
                                     cv2.CHAIN_APPROX_SIMPLE)
@@ -47,12 +49,16 @@ while True:
     for c in contours:
         (x, y, w, h) = cv2.boundingRect(c)
 
+        # Filtering out bounding boxes by their sizes
         if (5 <= w <= 150) and (15 <= h <= 120):
+            # Extracting image with the character and applying thresholding
+            # on it
             roi = gray[y:y + h, x:x + w]
             thresh = cv2.threshold(roi, 0, 255,
                                    cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
             (tH, tW) = thresh.shape
 
+            # Resizing by bigger dimension
             if tW > tH:
                 thresh = imutils.resize(thresh, width=32)
             else:
@@ -63,9 +69,11 @@ while True:
             # If NN needs to be provided with a RGB input image
             thresh = cv2.cvtColor(thresh, cv2.COLOR_GRAY2BGR)
 
+            # Determining padding, so 32x32 shape is preserved
             dX = int(max(0, 32 - tW) / 2.0)
             dY = int(max(0, 32 - tH) / 2.0)
 
+            # Padding the image
             padded = cv2.copyMakeBorder(thresh,
                                         top=dY, bottom=dY,
                                         left=dX, right=dX,
@@ -73,6 +81,7 @@ while True:
                                         value=(0, 0, 0))
             padded = cv2.resize(padded, (32, 32))
 
+            # Preparing padded image for classification
             padded = padded.astype("float32") / 255.0
             padded = np.expand_dims(padded, axis=-1)
 
@@ -81,8 +90,13 @@ while True:
     boxes = [b[1] for b in chars]
     chars = np.array([c[0] for c in chars], dtype="float32")
 
+    # Defining initial predictions shape in case of no characters were found
+    # to predict (100 dimension does not matter, it could be anything, only 36
+    # is important, which is equal to the number of classes)
     preds_shape = (100, 36)
 
+    # Trying to make a prediction, if there are no characters, predictions array
+    # is full of zeros and has shape (100, 36)
     try:
         preds = model.predict(chars)
     except UnboundLocalError:
@@ -102,9 +116,7 @@ while True:
 
     cv2.imshow("Frame", frame)
 
-    key = cv2.waitKey(1) & 0xFF
-
-    if key == ord('q'):
+    if cv2.waitKey(1) == ord('q'):
         break
 
     fps.update()
@@ -115,4 +127,3 @@ print(f"[INFO] Elapsed time in seconds: {fps.elapsed():.2f}")
 print(f"[INFO] Approx. FPS: {fps.fps():.2f}")
 
 cv2.destroyAllWindows()
-vs.stop()
